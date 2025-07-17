4d73b2fb761536a8bd79856086e5b167d6dd7957
Tony <87031977+TonyRVac4@users.noreply.github.com>
2025-07-17 18:53:06 +0300

    renamed update_bulk to update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class ABSRepository(ABC):
 
     @abstractmethod
-    def update_bulk(self, data: dict) -> dict:
+    def update(self, data: dict) -> dict:
         ...
 
     @abstractmethod
